a1cfc088bf93ef28f2b9b956d3076dff1df137eb
mikevatd3 <mike@datadrivendetroit.org>
2024-07-23 15:40:14 -0400

    added logging file

--- outlier/connection.py
+++ outlier/connection.py
@@ -1,3 +1,5 @@
 from sqlalchemy import create_engine
 
-tns_engine = create_engine("postgresql+psycopg2://michael@localhost:5432/investors")
+tns_engine = create_engine(
+    "postgresql+psycopg2://michael@localhost:5432/investors"
+)
--- outlier/gaussian_mixture.py
+++ outlier/gaussian_mixture.py
@@ -16,9 +16,10 @@ df = pd.read_parquet("temp_parcels.gzip")
 
 addresses = df.groupby(["clean_address"]).agg(
     num_parcels=("clean_address", "count"),
-    log_assessed_val=("assessed_value", log_assessed_val)
+    log_assessed_val=("assessed_value", log_assessed_val),
 )
 
+"""
 addresses_by_class = df.groupby(["clean_address", "super_class"]).agg(
     num_parcels=("clean_address", "count"),
     log_assessed_val=("assessed_value", log_assessed_val)
@@ -28,9 +29,26 @@ addresses_by_empty = df.groupby(["clean_address", "empty_lot"]).agg(
     num_parcels=("clean_address", "count"),
     log_assessed_val=("assessed_value", log_assessed_val)
 )
+"""
 
-# gmm = GaussianMixture(n_components=2, covariance_type="full")
+gmm = GaussianMixture(n_components=3, covariance_type="full")
+gmm.fit(addresses[["log_assessed_val"]])
 
+print(f"Model Weights: {gmm.weights_}")
+print(f"Model Means: {gmm.means_.flatten()}")
+print(f"Model covariances: {gmm.covariances_.flatten()}")
+print(f"Model aic: {gmm.aic(addresses[['log_assessed_val']])}")
+print(f"Model bic: {gmm.bic(addresses[['log_assessed_val']])}")
+
+ax = sn.kdeplot(gmm.sample(1000)[0], label="Model output")
+ax = sn.kdeplot(
+    addresses["log_assessed_val"].sample(1000), ax=ax, label="True distribution"
+)
+plt.legend()
+plt.show()
+
+
+"""
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 5))
 
 sample = addresses.sort_values("log_assessed_val")[CLIP:-CLIP].sample(5000) 
@@ -66,3 +84,4 @@ sn.histplot(
 )
 
 plt.show()
+"""
